35471d8c82d951123242aa28eb7347d68a7f0ce6
Administrator <admin@example.com>
2022-11-01 10:11:06 +0400

    Lab 1 ready

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def get_matrix_for_user(dataset_row):
                 result_matrix[row_states[i]] = dict()
                 result_matrix[row_states[i]][row_states[i + 1]] = 1
     fill_matrix_prob(result_matrix)
+    result_matrix['start'] = row_states[0]
     return result_matrix
 
 
@@ -27,16 +28,25 @@ def fill_matrix_prob(matrix):
             matrix[row_dict[0]][value[0]] = float(value[1]) / row_sum
 
 
-def print_decision_values_for_user(dataset_for_user, related_prob_matrix, gamma=0.05):
+def get_decision_array_for_user(user, dataset_for_user, related_prob_matrix, gamma=0.05):
+    decision_map = {}
+    decision_matrix = []
     row_states = np.array(dataset_for_user.split(';'))
+
+    if row_states[0] == related_prob_matrix['start']:
+        decision_matrix.append(False)
+
     for i in range(0, row_states.size - 1, 1):
         try:
             related_prob_matrix[row_states[i]][row_states[i + 1]]
         except KeyError:
-            print('False', end=' ')
+            decision_matrix.append(True)
             continue
-        print(f'{np.abs(related_prob_matrix[row_states[i]][row_states[i + 1]] > gamma)}', end=' ')
+        decision = np.abs(related_prob_matrix[row_states[i]][row_states[i + 1]] < gamma)
+        decision_matrix.append(decision)
     print('\n')
+    decision_map[user] = decision_matrix
+    return decision_map
 
 
 data                    = pd.read_csv("datasets/data.txt", sep=':')
@@ -49,11 +59,14 @@ for i in range(matrices.size):
     dataset_for_users[data['USER'][i]] = matrices[i]
 
 data_true = pd.read_csv("datasets/data_true.txt", sep=':')
+data_true_result = []
 for i in range(data_true.shape[0]):
-    print(f"Decisions for {data_true['USER'][i]}: ", end=' ')
-    print_decision_values_for_user(data_true['DATASET'][i], dataset_for_users[data_true['USER'][i]], 0.01)
+    data_true_result.append(get_decision_array_for_user(data_true['USER'][i], data_true['DATASET'][i], dataset_for_users[data_true['USER'][i]], 0.05))
 
+data_fake_result = []
 data_fake = pd.read_csv("datasets/data_fake.txt", sep=':')
 for i in range(data_fake.shape[0]):
-    print(f"Decisions for {data_fake['USER'][i]}: ", end=' ')
-    print_decision_values_for_user(data_fake['DATASET'][i], dataset_for_users[data_fake['USER'][i]], 0.01)
+    data_fake_result.append(get_decision_array_for_user(data_true['USER'][i], data_fake['DATASET'][i], dataset_for_users[data_fake['USER'][i]], 0.05))
+
+print(f'Data_True: {data_true_result}')
+print(f'Data_Fake: {data_fake_result}')
